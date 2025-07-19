670ede4d8a2977a1ae95493ba32ff09ff56574d2
Aman <amast4900@gmail.com>
2025-07-19 19:03:23 +0530

    Login Responsive done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,26 +2,37 @@ import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Events1 from './Components/Events1';
-import Course from './Components/Course'
-import {Footer} from './Components/Footer';
+import Course from './Components/Course';
+import { Footer } from './Components/Footer';
 import { Review } from './Components/Reviews';
 import CourseSection from './Components/CourseSection';
 import { Detailed } from './Components/Detailed';
+import SignIn from './Components/SignIn';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    
-   <>
-   <Header/>
-   <Main/>
-   <Course/>
-   <CourseSection/>
-   <Events1/>
-   <Detailed />
-   <Review />
-   <Footer/>
-   
-   </>
+    <Router>
+      <Header />
+      <Routes>
+        {/* Home Route */}
+        <Route path="/" element={
+          <>
+            <Main />
+            <Course />
+            <CourseSection />
+            <Events1 />
+            <Detailed />
+            <Review />
+            <Footer />
+          </>
+        } />
+
+        {/* SignIn Page Route */}
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PiStudentBold } from "react-icons/pi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-      <div className="logo"><span id="footerlogo"><PiStudentBold /></span>EduEventHub</div>
-      <nav className="nav">
+      <div className="logo">
+        <span id="footerlogo"><PiStudentBold /></span> EduEventHub
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <GiHamburgerMenu size={24} />
+      </div>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Courses</a></li>
-          <li><a href="#">Events</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">Login</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/events">Events</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/signin">Login</Link></li>
         </ul>
       </nav>
     </header>
