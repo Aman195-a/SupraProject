1f2409c527685c03413011eac8d3d9a302bfe37b
Aman <amast4900@gmail.com>
2025-07-19 13:38:26 +0530

    Ready

--- my-app/src/Components/Events1.jsx
+++ my-app/src/Components/Events1.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import "./Events1.css";
+
+import React from "react";
 
 const Events1 = () => {
   return (
-    <div>Events1</div>
-  )
-}
+    <>
+      <section className="services-container">
+        <h1> Our Services</h1>
 
-export default Events1
+        <div className="all-boxes">
+          {allshow.map((item, index) => (
+            <div className="box" key={index}>
+              <h2>{item.heading}</h2>
+              <p>{item.info}</p>
+              <div className="img-container">
+                <img src={item.img} alt={item.heading} />
+              </div>
+            </div>
+          ))}
+          ;
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Events1;
+
+const allshow = [
+  {
+    heading: "Explore Courses",
+    info: "choose from hundreds of online courses covering a variety of topics",
+    img: "/Image1.jpg",
+  },
+
+  {
+    heading: "Find Events",
+    info: "choose from hundreds of online courses covering a variety of topics",
+    img: "/Image2.jpg",
+  },
+
+  {
+    heading: "Make Network",
+    info: "choose from hundreds of online courses covering a variety of topics",
+    img: "/Image3.jpg",
+  },
+];
