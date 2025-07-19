1fd8dc2423a4ef8c65f25e4a53c17e1b7d55c12b
Aman <amast4900@gmail.com>
2025-07-19 12:04:35 +0530

    Add new files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Events1 from './Components/Events1';
+import Course from './Components/Course'
+import Footor from './Components/Footor';
+function App() {
+  return (
+   <>
+   <Header/>
+   <Main/>
+   <Course/>
+   <Events1/>
+   <Footor/>
+   </>
+  );
+}
+
+export default App;
